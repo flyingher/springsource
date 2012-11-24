f6f9e75b91853e16c9a090661a01e8d5b09a6659
flyingher <springsource@yeah.net>
2012-11-24 15:11:01 +0800

    add System(which has just a private constructor) bean test.

--- src/test/java/com/flyingh/demo/Demo.java
+++ src/test/java/com/flyingh/demo/Demo.java
@@ -14,6 +14,12 @@ import com.flyingh.vo.Worker;
 @SuppressWarnings("deprecation")
 public class Demo {
 	@Test
+	public void test3(){
+		System.out.println(System.lineSeparator().length());
+		System.out.println(new ClassPathXmlApplicationContext("beans.xml").getBean("system",System.class));
+	}
+	
+	@Test
 	public void test2() {
 		new XmlBeanFactory(new ClassPathResource("beans.xml"));
 		System.out.println("*************");
