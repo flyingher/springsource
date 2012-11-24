d430bfd90bdbaf966ff3a336449d0e44b2c826b5
flyingher <springsource@yeah.net>
2012-11-24 10:54:00 +0800

    add 3 VOs.

---
+++ src/main/java/com/flyingh/vo/Student.java
@@ -0,0 +1,14 @@
+package com.flyingh.vo;
+
+public class Student extends Person {
+	private float score;
+
+	public float getScore() {
+		return score;
+	}
+
+	public void setScore(float score) {
+		this.score = score;
+	}
+	
+}
---
+++ src/main/java/com/flyingh/vo/Worker.java
@@ -0,0 +1,14 @@
+package com.flyingh.vo;
+
+public class Worker extends Person {
+	private float salary;
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+
+}
