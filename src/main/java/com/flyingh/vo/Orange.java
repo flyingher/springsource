4e1c5b72399cde2d240ed82314b087a33be1e3a9
flyingher <springsource@yeah.net>
2012-11-25 15:26:07 +0800

    test the sequence of the InitializingBean,init-method,DisposableBean,destroy-method.

--- src/main/java/com/flyingh/vo/Orange.java
+++ src/main/java/com/flyingh/vo/Orange.java
@@ -1,6 +1,9 @@
 package com.flyingh.vo;
 
-public class Orange {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class Orange implements InitializingBean, DisposableBean {
 	private String name;
 
 	public Orange() {
@@ -19,11 +22,19 @@ public class Orange {
 
 	@SuppressWarnings("unused")
 	private void init() {
-		System.out.println("3.Orange.init()");
+		System.out.println("4.Orange.init()");
 	}
 
 	@SuppressWarnings("unused")
-	private void destroy() {
-		System.out.println("4.Orange.destroy()");
+	private void close() {
+		System.out.println("6.Orange.close()");
+	}
+
+	public void destroy() throws Exception {
+		System.out.println("5.Orange.destroy()");
+	}
+
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("3.Orange.afterPropertiesSet()");
 	}
 }
