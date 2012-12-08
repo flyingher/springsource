2951db78049e66624a8c41a2fdb435cd2a0d1798
flyingher <springsource@yeah.net>
2012-12-08 22:13:38 +0800

    add a parameter for the Exception in order to get the exception.

--- src/main/java/com/flyingh/aspect/ExceptionAspect.java
+++ src/main/java/com/flyingh/aspect/ExceptionAspect.java
@@ -1,7 +1,7 @@
 package com.flyingh.aspect;
 
 public class ExceptionAspect {
-	public void handleException() {
-		System.out.println("ExceptionAspect.handleException()");
+	public void handleException(Throwable e) {
+		System.out.println("ExceptionAspect.handleException()" + e);
 	}
 }
